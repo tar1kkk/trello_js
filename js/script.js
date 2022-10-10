46f8565fab50d055447e01716c159a3b797cf7b7
Taras <81424893+tar1kkk@users.noreply.github.com>
2022-10-10 16:50:44 +0300

    push full project

--- js/script.js
+++ js/script.js
@@ -43,6 +43,7 @@ function addTask() {
 		value = '';
 		form.style.display = 'none';
 		btn.style.display = 'block';
+		dragNdrop()
 	})
 
 }
@@ -56,9 +57,13 @@ function addBoard() {
 	<span contenteditable="true" class="title">Введите название</span>
 	<div class="list"></div>
 	`
+	board.addEventListener('dblclick', () => {
+		board.remove();
+	})
 	boards.append(board);
 
 	changeTitle()
+	dragNdrop()
 }
 button.addEventListener('click', addBoard);
 
@@ -73,3 +78,49 @@ function changeTitle() {
 
 }
 changeTitle()
+
+let draggedItem = null;
+
+function dragNdrop() {
+	const listItems = document.querySelectorAll('.list_item');
+	const lists = document.querySelectorAll('.list');
+
+	for (let i = 0; i < listItems.length; i++) {
+		const item = listItems[i];
+
+		item.addEventListener('dragstart', () => {
+			draggedItem = item;
+			setTimeout(() => {
+				item.style.display = 'none';
+			}, 0)
+		})
+		item.addEventListener('dragend', () => {
+			setTimeout(() => {
+				item.style.display = 'block';
+				draggedItem = null;
+			}, 0)
+		})
+
+		item.addEventListener('dblclick', () => {
+			item.remove();
+		})
+		for (let j = 0; j < lists.length; j++) {
+			const list = lists[j];
+			list.addEventListener('dragover', (e) => {
+				e.preventDefault();
+			})
+			list.addEventListener('dragenter', function (e) {
+				e.preventDefault();
+				this.style.backgroundColor = 'rgba(0,0,0,.3)';
+			})
+			list.addEventListener('dragleave', function (e) {
+				this.style.backgroundColor = 'rgba(0,0,0,0)';
+			})
+			list.addEventListener('drop', function (e) {
+				this.style.backgroundColor = 'rgba(0,0,0,0)';
+				this.append(draggedItem);
+			})
+		}
+	}
+}
+dragNdrop()
